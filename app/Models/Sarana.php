66ff694fe1f47dd0be2e521105a829baf5adc132
bagusyanuar <bagus.yanuar613@gmail.com>
2022-10-17 13:13:01 +0700

    database

---
+++ app/Models/Sarana.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sarana extends Model
+{
+    use HasFactory;
+
+    protected $table = 'sarana';
+
+    protected $fillable = [
+        'name',
+        'qty'
+    ];
+}
